58d77dcfa8c0fe7def1118141cbea7c2f8b6d16c
shivaban <shivanibansal020996@gmail.com>
2024-07-28 11:18:37 +0530

    added gateway

---
+++ HotelConsole/src/main/java/com/shivani/HotelConsole/HotelConsoleApplication.java
@@ -0,0 +1,13 @@
+package com.shivani.HotelConsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelConsoleApplication.class, args);
+	}
+
+}
---
+++ HotelConsole/src/test/java/com/shivani/HotelConsole/HotelConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivani.HotelConsole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelConsoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
